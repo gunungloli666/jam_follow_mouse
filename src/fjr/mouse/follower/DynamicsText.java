1de03b2ec8a2a9a6b715c9e0102148fd7cdd556c
gunungloli666 <gunungloli@gmail.com>
2014-03-16 13:41:22 +0700

    sudah dirubah text...

--- src/fjr/mouse/follower/DynamicsText.java
+++ src/fjr/mouse/follower/DynamicsText.java
@@ -4,8 +4,24 @@ import javafx.scene.text.Text;
 
 public class DynamicsText extends Text{
 	
+	double offsetx; double offsety; 
 	
-	
-	public DynamicsText(){	
+	public DynamicsText(String text, double x, double y){
+		super(text); 
+		this.offsetx = x; 
+		this.offsety = y; 
 	}
+	
+	public void setOffSet(double x, double y){
+		this.offsetx = x; 
+		this.offsety = y; 
+	}
+	
+	public void move(double x, double y){
+		super.setTranslateX(offsetx + x); 
+		super.setTranslateY(offsety + y);
+	}
+	
+	public double getOffsetX(){return offsetx; }
+	public double getOffsetY(){return offsety ; }
 }
--- src/fjr/mouse/follower/MouseFollowerPlugin.java
+++ src/fjr/mouse/follower/MouseFollowerPlugin.java
@@ -15,7 +15,6 @@ import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.StackPane;
 import javafx.scene.text.Font;
 import javafx.scene.text.FontWeight;
-import javafx.scene.text.Text;
 import javafx.util.Duration;
 
 public class MouseFollowerPlugin extends StackPane{
@@ -29,18 +28,18 @@ public class MouseFollowerPlugin extends StackPane{
 	Group root; 
 	
 	
-	ArrayList<Text> allList = new ArrayList<>(); 
+	ArrayList<DynamicsText> allList = new ArrayList<>(); 
 	
-	ArrayList<Text> textDate = new ArrayList<>(); 
-	ArrayList<Text> textMonth = new ArrayList<>(); 
-	ArrayList<Text> textYear = new ArrayList<>(); 
-	ArrayList<Text> day = new ArrayList<>();
+	ArrayList<DynamicsText> textDate = new ArrayList<>(); 
+	ArrayList<DynamicsText> textMonth = new ArrayList<>(); 
+	ArrayList<DynamicsText> textYear = new ArrayList<>(); 
+	ArrayList<DynamicsText> day = new ArrayList<>();
 	
-	ArrayList<Text> textHours = new ArrayList<>();
-	ArrayList<Text> textMinute = new ArrayList<>(); 
-	ArrayList<Text> textSecond = new ArrayList<>(); 
+	ArrayList<DynamicsText> textHours = new ArrayList<>();
+	ArrayList<DynamicsText> textMinute = new ArrayList<>(); 
+	ArrayList<DynamicsText> textSecond = new ArrayList<>(); 
 	
-	ArrayList<Text> combineDate = new ArrayList<>(); 
+	ArrayList<DynamicsText> combineDate = new ArrayList<>(); 
 	
 	Timeline animasi; 
 	
@@ -127,69 +126,65 @@ public class MouseFollowerPlugin extends StackPane{
 		root.getChildren().removeAll(combineDate);
 		combineDate.clear();
 		for(String s : date[0].split("")){
-			Text t = new Text(s);
+			DynamicsText t = new DynamicsText(s, offsetX , offsetY );
 			combineDate.add(t); 
 		}
 		for(int i=0; i< 1; i++){
-			Text t = new Text(" ");
+			DynamicsText t = new DynamicsText(" ", offsetX, offsetY);
 			combineDate.add(t); 
 		}
 		for(String s : date[1].split("")){
-			Text t = new Text(s);
+			DynamicsText t = new DynamicsText(s , offsetX, offsetY);
 			combineDate.add(t); 
 		}
 		for(int i=0; i< 1; i++){
-			Text t = new Text(" ");
+			DynamicsText t = new DynamicsText(" ", offsetX, offsetY );
 			combineDate.add(t); 
 		}
 		for(String s : date[2].split("")){
-			Text t = new Text(s);
+			DynamicsText t = new DynamicsText(s , offsetX, offsetY);
 			combineDate.add(t); 
 		}
 		for(int i=0; i< 1; i++){
-			Text t = new Text(" ");
+			DynamicsText t = new DynamicsText(" ", offsetX , offsetY );
 			combineDate.add(t); 
 		}
 		for(String s : date[3].split("")){
-			Text t = new Text(s);
+			DynamicsText t = new DynamicsText(s,  offsetX,  offsetY);
 			combineDate.add(t); 
 		}
 		for(int i=0; i< 1; i++){
-			Text t = new Text(" ");
+			DynamicsText t = new DynamicsText(" " , offsetX , offsetY );
 			combineDate.add(t); 
 		}
-		
 		root.getChildren().addAll(combineDate);
 	}
 	
 	public void setTextPosition(double initAngle){
 		int size = combineDate.size(); 
-		double x0 = offsetX; 
-		double y0 = offsetY; 
 		double angleStep = (2 * Math.PI)/size; 
 		double angle = initAngle; 
-		for(Text t : combineDate){
-			double x = x0 + radius * Math.cos(angle); 
-			double y = y0 + radius * Math.sin(angle); 
-			t.setTranslateX(x);
-			t.setTranslateY(y);
+		for(DynamicsText t : combineDate){
+			double x = radius * Math.cos(angle); 
+			double y = radius * Math.sin(angle); 
+			t.move(x, y);
 			angle -= angleStep;
 		}
 	}
 	
 	public void initClockList(){
 		for(int i = 0; i < 13; i++){
-			Text text  =  new Text(".");
+			DynamicsText text  =  new DynamicsText("." ,  offsetX,  offsetY );
 			text.setFont(Font.font(null, FontWeight.BOLD, 14));
 			textSecond.add(text); 
 		}
 		for(int i = 0; i < 10; i++){
-			Text text = new Text(".");
+			DynamicsText text = new DynamicsText("." , offsetX, offsetY);
 			text.setFont(Font.font(null, FontWeight.BOLD, 14));
 			textMinute.add(text); 
 		}
 		for(int i = 0; i < 7; i++){
-			Text text = new Text(".");
+			DynamicsText text = new DynamicsText("." , offsetX, offsetY);
 			text.setFont(Font.font(null, FontWeight.BOLD, 14));
 			textHours.add(text); 
 		}
@@ -203,21 +198,23 @@ public class MouseFollowerPlugin extends StackPane{
 	double minuteAngle; 
 	double hoursAngle; 
 
-	ArrayList<Text> listLabel = new ArrayList<>(); 
+	ArrayList<DynamicsText> listLabel = new ArrayList<>(); 
 	
 	public void addClockLabel(){
 		double radius = this.radius - 22;
 		double step = TWO_PI / 12.; 
 		double angle0 = - Math.PI/ 2 + step ; 
 		for(int i=1; i<=12; i++){
-			Text t  = new Text(Integer.toString(i)); 
-			t.setTranslateX(offsetX + radius * Math.cos( angle0));
-			t.setTranslateY(offsetY + radius * Math.sin(angle0)); 
+			DynamicsText t  = new DynamicsText(Integer.toString(i), offsetX, offsetY); 
+			double x = radius * Math.cos(angle0); 
+			double y = radius * Math.sin(angle0); 
+			t.move(x, y);
 			angle0 += step; 
 			listLabel.add(t); 
 		}
 		
 		root.getChildren().addAll(listLabel);
+		allList.addAll(listLabel); 
 	}
 	
 	public void setInitClockPotition(){
@@ -251,25 +248,28 @@ public class MouseFollowerPlugin extends StackPane{
 		double radius  = this.radius - 30; 
 		double secondsRadiusStep  = (radius-5 ) / textSecond.size(); 
 		double secondsRadius = 0; 
-		for(Text t : textSecond){
-			t.setTranslateX(offsetX + secondsRadius * Math.cos(secondAngle));
-			t.setTranslateY(offsetY + secondsRadius * Math.sin(secondAngle));
+		for(DynamicsText t : textSecond){
+			double x = secondsRadius * Math.cos(secondAngle); 
+			double y = secondsRadius * Math.sin(secondAngle); 
+			t.move(x, y);
 			secondsRadius += secondsRadiusStep; 
 		}
 		
 		double  minuteRadiusStep =(radius - 10 ) / textMinute.size(); 
 		double minuteRadius = 0; 
-		for(Text t : textMinute ){
-			t.setTranslateX(offsetX + minuteRadius * Math.cos(minuteAngle));
-			t.setTranslateY(offsetY+ minuteRadius * Math.sin(minuteAngle));
+		for(DynamicsText t : textMinute ){
+			double x = minuteRadius * Math.cos(minuteAngle); 
+			double y = minuteRadius * Math.sin(minuteAngle); 
+			t.move(x, y);
 			minuteRadius += minuteRadiusStep; 
 		}
 		
 		double hoursRadiusStep = (radius - 10 ) / textHours.size();
 		double hoursRadius = 0; 
-		for( Text t : textHours){
-			t.setTranslateX(offsetX + hoursRadius * Math.cos(hoursAngle));
-			t.setTranslateY(offsetY + hoursRadius * Math.sin(hoursAngle));
+		for( DynamicsText t : textHours){
+			double x = hoursRadius * Math.cos(hoursAngle); 
+			double y = hoursRadius * Math.sin(hoursAngle); 
+			t.move(x, y);
 			hoursRadius += hoursRadiusStep; 
 		}
 	}
